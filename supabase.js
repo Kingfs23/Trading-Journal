8c37180ae2db2c97e043e069c2b8fb1fd51cc508
Kingfs23 <kingfx982@gmail.com>
2026-08-15 19:55:17 +0100

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = "https://nfmnbwyluttivkgplmrh.supabase.co";
-const supabaseKey = "sb_publishable_fEGUHINYAMku5Bl_N3ebTA_vZcqV_j3";
-
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
